e7f9d854fed3b265095a7459691657762a584782
Dmitry <bekabot27@mail.ru>
2026-01-08 14:17:56 +0300

    ПР2

--- testing/testing.sh
+++ testing/testing.sh
@@ -1,31 +1,45 @@
 #!/bin/bash
 
-# Массив входных данных
-in_array=(
-"2"
-"1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1"
+# Путь к бинарнику
+BINARY="../PR_1.0/usr/local/bin/TRRSPO"
+
+if [ ! -f "$BINARY" ]; then
+  echo "Ошибка: бинарник не найден по пути $BINARY"
+  exit 1
+fi
+
+# Массив входных данных для тестирования
+input_array=(
+  "1 2 3 4 5 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1  # пример для матрицы 5x5"
+  "2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2  # пример для матрицы 6x6"
 )
 
 # Массив ожидаемых результатов
-out_array=(
-"Матрица"
-"Среднее арифметическое"
+expected_array=(
+  "матрицы после замены элементов на среднее арифметическое" 
+  "матрицы после замены элементов на среднее арифметическое"
 )
 
-# Путь к программе
-PROGRAM="../usr/local/bin/TRRSPO"
+# Счётчик ошибок
+errors=0
 
-for i in 0 1
-do
-    result=$(echo "${in_array[$i]}" | $PROGRAM)
 
-    if echo "$result" | grep -q "${out_array[$i]}"; then
-        echo "Test $i: OK"
-    else
-        echo "Test $i: FAIL"
-        exit 1
-    fi
+for i in "${!input_array[@]}"; do
+  result=$(echo "${input_array[$i]}" | "$BINARY")
+
+  if [ "$result" == "${expected_array[$i]}" ]; then
+    echo "Тест $((i+1)) ✅ OK"
+  else
+    echo "Тест $((i+1)) ❌ Fail"
+    echo "Ожидалось: ${expected_array[$i]}"
+    echo "Получено:  $result"
+    errors=$((errors+1))
+  fi
 done
 
-exit 0
+if [ $errors -ne 0 ]; then
+  exit 1
+fi
+
+echo "Все тесты пройдены успешно!"
 
